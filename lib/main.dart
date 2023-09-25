ec274adbfca74fdc623ec6a8f7cc8f58ec6ec3c4
Augusto Salazar SIlva <augustosalazar@uninorte.edu.co>
2023-09-25 14:17:46 -0500

    Add loggy to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:f_data_crud/ui/controllers/data_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:loggy/loggy.dart';
 
 import 'ui/my_app.dart';
 
 void main() {
+  Loggy.initLoggy(
+    logPrinter: const PrettyPrinter(
+      showColors: true,
+    ),
+  );
   Get.put(DataController());
   runApp(const MyApp());
 }
